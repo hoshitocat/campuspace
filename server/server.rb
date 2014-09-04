74b1d3745fca4e390b0d1b3e37267fe42a57ebab
@h0shim0 <micky12127@gmail.com>
2014-09-04 13:16:25 +0900

    created database

--- server/server.rb
+++ server/server.rb
@@ -1,16 +1,32 @@
+# config:utf-8
+require 'mysql2'
+require 'active_record'
 require 'sinatra'
 require 'json'
 require 'sinatra/cross_origin'
 
+ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
+ActiveRecord::Base.establish_connection(:development)
+
+class User < ActiveRecord::Base
+end
+
+class Event < ActiveRecord::Base
+end
+
 enable :cross_origin
-get '/show' do
-  user = {
-    id: 1,
-    name: "ryohei hoshi",
-    age: 22
-  }
+get '/users.json' do
+  content_type :json, :charset => 'utf-8'
+  users = User.all
   cross_origin
-  user.to_json
+  users.to_json
+end
+
+get '/getEvents.json' do
+  content_type :json, :charset => 'utf-8'
+  events = Event.all
+  cross_origin
+  events.to_json
 end
 
 post '/edit' do
